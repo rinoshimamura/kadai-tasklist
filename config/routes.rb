924555f51bc1e8cc7f032cb8870bf8453243c6b8
rinoshim <ricochoco1998@gmail.com>
2018-10-30 14:53:25 +0000

    add column

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
-  
+  # リクエストをどのコントローラで処理するかの道標
   resources :tasks
+  
+  # resources :posts
+  # /posts       => posts#index
+  # /posts/:id   => posts#show
+  # /posts(POST) => posts#create
+  # /posts/:id(DELETE) => posts#destroy
+  # /posts/:id(PUT)    => posts#update
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181027060442) do
+ActiveRecord::Schema.define(version: 20181029165939) do
 
   create_table "task2s", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20181027060442) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "status"
   end
 
 end
